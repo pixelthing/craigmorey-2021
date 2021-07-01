4071ba8076f2c1861a9e0cddaec1c27b190f338d
pixelthing <craigmorey@gmail.com>
2021-07-01 23:13:15 +0200

    fix mag subsequent pages

--- js/magazine.js
+++ js/magazine.js
@@ -14,7 +14,7 @@ const magazine = function() {
     const mq = window.matchMedia( "(min-width: 40em) and (min-height: 20rem)" );
     // to enable magazine layout, the viewport needs to pass 
     // the above media query and have the ".mag" class on the body
-    if (mq.matches && document.querySelector('body').classList.contains('mag')) {
+    if (mq.matches && document.querySelector('html').classList.contains('mag')) {
       console.log(`MAG SET-UP`);
       initPageCounter();
       flowText(1, observer);
@@ -36,81 +36,80 @@ const magazine = function() {
     }
   }
 
-
-const flowText = (pageId,callBack) => {
-//  console.log('*** page' + pageId , '-----')
-  const maxPages = 50;
-  if (pageId > maxPages) {
-    console.error(`MAX ${maxPages} PAGES!`);
-    return;
-  }
-  // grab the paras in the target page
-  const prePage = document.querySelector('#page' + pageId);
-  const prePageNodes = prePage.children;
-  const prePageNodesLength = prePageNodes.length;
-//  console.log('*** page' + pageId, 'prePageNodes', prePageNodes.length)
-  // get the page height (determined by CSS)
-  const prePageMetrics = prePage.getBoundingClientRect();
-  const prePageRight = prePageMetrics.right;
-  const prePageBottom = prePageMetrics.bottom;
-//  console.log('*** page' + pageId, 'container width/height', prePageRight, prePageBottom);
-  // create a new page, ready for any overflow paras
-  const newPage = document.createElement('div');
-  const newPageId = pageId + 1;
-  newPage.id = 'page' + newPageId;
-  newPage.classList.add('mag__page');
-  // construct a list of paras to move into next page
-  let   overflowFound = 0;
-  for(let i = 0; i < prePageNodesLength; i++) {
-    const el = prePageNodes[i];
-    if (!el || (el.tagName === 'A' && el.innerText === '#')) {
-      continue;
+  const flowText = (pageId,callBack) => {
+  //  console.log('*** page' + pageId , '-----')
+    const maxPages = 50;
+    if (pageId > maxPages) {
+      console.error(`MAX ${maxPages} PAGES!`);
+      return;
     }
-    const elMetrics = el.getBoundingClientRect();
-    const elRight = elMetrics.right;
-    const elBottom = elMetrics.bottom
-    if (!overflowFound && elRight > prePageRight) {      
-//      console.log('*** page' + pageId, 'el dims', el.tagName, el.innerText.substr(0,5), elRight, elBottom, overflowFound)
-      overflowFound = i;
+    // grab the paras in the target page
+    const prePage = document.querySelector('#page' + pageId);
+    const prePageNodes = prePage.children;
+    const prePageNodesLength = prePageNodes.length;
+  //  console.log('*** page' + pageId, 'prePageNodes', prePageNodes.length)
+    // get the page height (determined by CSS)
+    const prePageMetrics = prePage.getBoundingClientRect();
+    const prePageRight = prePageMetrics.right;
+    const prePageBottom = prePageMetrics.bottom;
+  //  console.log('*** page' + pageId, 'container width/height', prePageRight, prePageBottom);
+    // create a new page, ready for any overflow paras
+    const newPage = document.createElement('div');
+    const newPageId = pageId + 1;
+    newPage.id = 'page' + newPageId;
+    newPage.classList.add('mag__page');
+    // construct a list of paras to move into next page
+    let   overflowFound = 0;
+    for(let i = 0; i < prePageNodesLength; i++) {
+      const el = prePageNodes[i];
+      if (!el || (el.tagName === 'A' && el.innerText === '#')) {
+        continue;
+      }
+      const elMetrics = el.getBoundingClientRect();
+      const elRight = elMetrics.right;
+      const elBottom = elMetrics.bottom
+      if (!overflowFound && elRight > prePageRight) {      
+  //      console.log('*** page' + pageId, 'el dims', el.tagName, el.innerText.substr(0,5), elRight, elBottom, overflowFound)
+        overflowFound = i;
+      }
+      if (overflowFound) {
+        // add to the next page
+        newPage.appendChild(el.cloneNode(true));
+      }
+    };
+    if (!overflowFound) {
+      // add the counter total
+      document.querySelector('.mag-counter__total').innerText = pageId;
+      document.querySelector('.mag-counter').classList.add('mag-counter--active');
+      console.log('*** page' + pageId + ' COMPLETE');
+      callBack();
+      return;
     }
-    if (overflowFound) {
-      // add to the next page
-      newPage.appendChild(el.cloneNode(true));
+    prePage.after(newPage);
+    
+    // add to the page counter
+    const counter = document.createElement('span');
+    counter.innerText = pageId + 1;
+    document.querySelector('.mag-counter__current__inner').append(counter);
+  //  console.log('*** page' + pageId, '-----', overflowFound, prePageNodesLength)
+    // remove from the previous page
+    for(let i = prePageNodesLength-1; i >= overflowFound; i--) {
+      const el = prePageNodes[i];
+      try {
+        prePage.removeChild(el);
+      } catch (err) {
+  //      console.error('*** page' + pageId + ' ERROR', i, prePageNodesLength, err, el.tagName, el.innerText.substr(0,5))
+      }
     }
-  };
-  if (!overflowFound) {
-    // add the counter total
-    document.querySelector('.mag-counter__total').innerText = pageId;
-    document.querySelector('.mag-counter').classList.add('mag-counter--active');
-    console.log('*** page' + pageId + ' COMPLETE');
-    callBack();
-    return;
-  }
-  prePage.after(newPage);
-  
-  // add to the page counter
-  const counter = document.createElement('span');
-  counter.innerText = pageId + 1;
-  document.querySelector('.mag-counter__current__inner').append(counter);
-//  console.log('*** page' + pageId, '-----', overflowFound, prePageNodesLength)
-  // remove from the previous page
-  for(let i = prePageNodesLength-1; i >= overflowFound; i--) {
-    const el = prePageNodes[i];
-    try {
-      prePage.removeChild(el);
-    } catch (err) {
-//      console.error('*** page' + pageId + ' ERROR', i, prePageNodesLength, err, el.tagName, el.innerText.substr(0,5))
-    }
-  }
-  // run on this latest para to see if extra pages need to be created.
-  // Double RAF it to push the next batch of measurements 
-  // into the next frame budget.
-  window.requestAnimationFrame(() => {
+    // run on this latest para to see if extra pages need to be created.
+    // Double RAF it to push the next batch of measurements 
+    // into the next frame budget.
     window.requestAnimationFrame(() => {
-      flowText(newPageId, callBack); 
-    });  
-  });
-}
+      window.requestAnimationFrame(() => {
+        flowText(newPageId, callBack); 
+      });  
+    });
+  }
 
   const initPageCounter = () => {
     const counterContainer = document.createElement('div');
